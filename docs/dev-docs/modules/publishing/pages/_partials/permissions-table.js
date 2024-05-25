43e47844914032b711e74a0b65ce06823f29052c
Vandit Singh <vanditsinghkv@gmail.com>
2024-05-25 15:53:08 +0530

    add prettierrc and prettierignore

--- docs/dev-docs/modules/publishing/pages/_partials/permissions-table.js
+++ docs/dev-docs/modules/publishing/pages/_partials/permissions-table.js
@@ -1,36 +1,36 @@
 $(function () {
-  $("#permissions").DataTable({
+  $('#permissions').DataTable({
     ajax: {
-      url: "https://reports.jenkins.io/github-jenkinsci-permissions-report.json",
-      dataSrc: "",
+      url: 'https://reports.jenkins.io/github-jenkinsci-permissions-report.json',
+      dataSrc: '',
     },
     columns: [
       {
-        title: "Repository",
+        title: 'Repository',
         render: function (data, type, row, metadata) {
           return (
             '<a href="https://github.com/jenkinsci/' +
             data +
             '" target="_blank" rel="noreferrer noopener">' +
             data +
-            "</a>"
+            '</a>'
           );
         },
       },
       {
-        title: "User",
+        title: 'User',
         render: function (data, type, row, metadata) {
           return (
             '<a href="https://github.com/' +
             data +
             '" target="_blank" rel="noreferrer noopener">' +
             data +
-            "</a>"
+            '</a>'
           );
         },
       },
       {
-        title: "Access",
+        title: 'Access',
       },
     ],
   });
